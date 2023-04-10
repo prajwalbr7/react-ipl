df9f4a717f46e6071774c2f8aec4904bc4c42912
Prajwal Yadav B R <78904638+prajwalbr7@users.noreply.github.com>
2023-04-10 13:20:22 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,12 @@
 // Write your code here
 import './index.css'
 import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 import TeamCard from '../TeamCard'
 
 class Home extends Component {
-  state = {Logos: []}
+  state = {Logos: [], isLoading: true}
 
   componentDidMount() {
     this.getLogoCardItems()
@@ -19,26 +21,39 @@ class Home extends Component {
       name: eachItem.name,
       teamImageUrl: eachItem.team_image_url,
     }))
-    this.setState({Logos: UpdateData})
+    this.setState({Logos: UpdateData, isLoading: false})
   }
 
   render() {
-    const {Logos} = this.state
+    const {Logos, isLoading} = this.state
     return (
-      <div className="container1">
-        <div className="container-img-heading">
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/ipl-logo-img.png"
-            alt="ipl logo"
-            className="ipl-logo-size"
-          />
-          <h1 className="heading-ipl-logo-style">IPL Dashboard</h1>
-        </div>
-        <ul className="ul-style">
-          {Logos.map(eachItem => (
-            <TeamCard Details={eachItem} key={eachItem.id} />
-          ))}
-        </ul>
+      <div>
+        {isLoading ? (
+          <div data-testid="loader" className="loader-container">
+            <Loader
+              type="BallTriangle"
+              color="#00BFFF"
+              height={80}
+              width={80}
+            />
+          </div>
+        ) : (
+          <div className="container1">
+            <div className="container-img-heading">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/ipl-logo-img.png"
+                alt="ipl logo"
+                className="ipl-logo-size"
+              />
+              <h1 className="heading-ipl-logo-style">IPL Dashboard</h1>
+            </div>
+            <ul className="ul-style">
+              {Logos.map(eachItem => (
+                <TeamCard Details={eachItem} key={eachItem.id} />
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     )
   }
